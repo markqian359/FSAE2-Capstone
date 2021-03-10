e76b8f132e91f53a14dccc5aec767c522edd04da
Zhiwei Qian <67033815+markqian359@users.noreply.github.com>
2021-03-09 21:50:25 -0500

    Update FSAE2 - Full Arduino Code.ino

--- FSAE2 - Full Arduino Code.ino
+++ FSAE2 - Full Arduino Code.ino
@@ -143,3 +143,26 @@ void loop() {
   Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
   Serial.println();
 }
+
+// PLX-DAQ Data Logging
+unsigned long int milli_time;    //variable to hold the time
+float voltage;         //variable to hold the voltage form A0
+void setup() {
+  Serial.begin(128000);               //Fastest baudrate
+  Serial.println("CLEARDATA");        //This string is defined as a 
+                                      // commmand for the Excel VBA 
+                                      // to clear all the rows and columns
+  Serial.println("LABEL,Computer Time,Time (Milli Sec.),Volt");  
+                                      //LABEL command creates label for 
+                                      // columns in the first row with bold font
+}
+void loop() {
+  milli_time = millis();
+  voltage = 5.0 * analogRead(A0) / 1024.0;
+  Serial.print("DATA,TIME,");
+  Serial.print(milli_time);
+  Serial.print(",");
+  Serial.println(voltage);
+    
+  delay(100);                    //Take samples every one second
+}
