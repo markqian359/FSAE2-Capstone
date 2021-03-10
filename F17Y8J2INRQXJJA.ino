62e22d71c7a6fc5316d335464ae1cec64c3a2524
Zhiwei Qian <67033815+markqian359@users.noreply.github.com>
2021-03-09 21:42:02 -0500

    Update F17Y8J2INRQXJJA.ino

--- F17Y8J2INRQXJJA.ino
+++ F17Y8J2INRQXJJA.ino
@@ -73,4 +73,18 @@ void loop(){
 
 }
   
+// Displacement Sensor - To Be Changed
+int sensorPin = 0; //analog pin 0
 
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  int val = analogRead(sensorPin);
+  Serial.println(val);
+
+  //just to slow down the output - remove if trying to catch an object passing by
+  delay(100);
+
+}
