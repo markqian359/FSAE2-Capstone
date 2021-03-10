054d9ccf3a03b103127f829f1127db0d5c3b4369
Zhiwei Qian <67033815+markqian359@users.noreply.github.com>
2021-03-09 21:48:28 -0500

    Update and rename F17Y8J2INRQXJJA.ino to FSAE2 - Full Arduino Code.ino

--- FSAE2 - Full Arduino Code.ino
+++ FSAE2 - Full Arduino Code.ino
@@ -88,3 +88,58 @@ void loop(){
   delay(100);
 
 }
+
+// Thermistor
+int ThermistorPin = 0;
+int Vo;
+float R1 = 10000;
+float logR2, R2, T;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+
+  Vo = analogRead(ThermistorPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  T = T - 273.15;
+  //T = (T * 9.0)/ 5.0 + 32.0; 
+
+  Serial.print("Temperature: "); 
+  Serial.print(T);
+  Serial.println(" C"); 
+
+  delay(500);
+}
+
+// Load Cell Code - To be edited
+#include "HX711.h"
+
+#define calibration_factor -7050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+
+#define LOADCELL_DOUT_PIN  3
+#define LOADCELL_SCK_PIN  2
+
+HX711 scale;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("HX711 scale demo");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+  Serial.println("Readings:");
+}
+
+void loop() {
+  Serial.print("Reading: ");
+  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
+  Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
+  Serial.println();
+}
